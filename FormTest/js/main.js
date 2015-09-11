afca02d261a07730f32fe3058071e6ab36b80377
Rachel Broering <rachelbroering@Rachels-MacBook-Pro-2.local>
2015-09-10 23:27:32 -0400

    Last commit for the form application i did

--- FormTest/js/main.js
+++ FormTest/js/main.js
@@ -1,15 +1,14 @@
 /*jslint browser: true, devel: true*/
 $(document).ready(function () {
   Parse.$ = jQuery;
-  
+
   Parse.initialize("vNGHN5aZTRjQxsuFV4cX4ZTIVCFHRlCJKQm0O6Qy", "JzDD3z7R0H7JsLezVM70t9rMnETN3YxAtQczlMEi");
-  
+
   //------
   //Models
   //------
-  
+
   var Form = Parse.Object.extend("Form");
-  var form = new Form();
   
   var nameInputField = document.getElementById("nameInput");
   var addressInputField = document.getElementById("addressInput");
@@ -19,212 +18,198 @@ $(document).ready(function () {
   var imageInputField = document.getElementById("imageInput");
 
   var submitValues = document.getElementById('submitBtn');
-  submitValues.onclick = function () {
-    form.set("name", nameInputField.value);
-    form.set("address", addressInputField.value);
-    form.set("phone", number.value);
-    form.set("email", emailInputField.value);
-    
-    form.save(null, {
-      success: function (form) {
-        //Execute any logic
-        alert('New Form object was created!');
-        
-        addressInputField.value = "";
-        number.value = "";
-        emailInputField.value = "";
-        imageInputField.value = "";
-      },
-      
-      error: function (form, error) {
-        //Execute any logic
-        //Error is a Parse.Error with an error code & message
-        alert('Failed to create a new Form object, with error code ' + error.message);
-      }
-    });
-    
-    
-    
-    
-    console.log('The info is being retrieved.');
-    var query = new Parse.Query(Form);
-    query.equalTo("name", nameInputField);
-    query.find({
-      success: function (results) {
-        alert('Your info has been retrieved!!');
-      },
-      
-      error: function (error) {
-        alert('There was a problem with retrieving your info.');
-      }
-    });
-  };
-  
-    function saveImage(objParseFile) {
-      var profileImage = new Parse.Object("ProfileImage");
-      profileImage.set("name", nameInputField.value);
-      profileImage.set("profileImg", objParseFile);
-      profileImage.save(null, {
-        success: function (profilePic) {
-          //Execute any logic
-          console.log('New image object saved');
-          var photo = profilePic.get("profileImg");
-          $("#profileImg")[0].src = photo.url();
-          
-          nameInputField.value = "";
+  submitValues.onclick = function (event) {
+
+    event.preventDefault();
+    var nameInputField = $("#nameInput").val();
+    var addressInputField = $("#addressInput").val();
+    var phoneInputField = $("#phoneInput").val();
+    var number = $("#phoneInput").val();
+    var emailInputField = $("#emailInput").val();
+    var imageInputField = $("#imageInput").val();
+
+    var newForm = new Form();
+    newForm.set("name", nameInputField);
+    newForm.set("address", addressInputField);
+    newForm.set("phone", number);
+    newForm.set("email", emailInputField);
+    newForm.set("user", Parse.User.current());
+
+    //Here we are getting the file from the input form
+    var fileElement = $('#imageInput')[0];
+    var filePath = $('#imageInput').val();
+    var fileName = filePath.split('\\').pop();
+
+    if (fileElement.files.length > 0) {
+      var file = fileElement.files[0];
+      var newFile = new Parse.File(fileName, file);
+
+      newFile.save({
+        success: function () {
+          console.log('File saved successfully');
         },
-        
-        error: function (profilePic, error) {
-          //Execute any logic
-          //error is a Parse.Error with an error code and description
-          alert('Failed to save image, with error code: ' + error.description);
+
+        error: function (file, error) {
+          console.log('File Save Error: ' + error.message);
         }
-        
+      }).then(function (theFile) {
+        newForm.set("file", theFile);
+
+        newForm.save({
+          success: function () {
+            console.log('The form was saved with the file.');
+          },
+
+          error: function (error) {
+            console.log('The form did not save with the file, Error: ' + error.message);
+          }
+        });
+
       });
     }
-      
-      $('#imageInput').bind("change", function (e) {
-        var fileUploadControl = $('#imageInput')[0];
-        var file = fileUploadControl.files[0];
-        var name = file.name;
-        var parseFile = new Parse.File(name, file);
-        
-        parseFile.save().then
-        (function () {
-          saveImage(parseFile);
+    else {
+
+      newForm.save({
+        success: function () {
+          console.log('The form was successfully retrieved!');
           
-          nameInputField.value = "";
-    
+          //This is where the view should refresh, so the newly added form is shown
         },
-        function (error) {
-          alert('error');
+
+        error: function (error) {
+          console.log('Error: ' + error.message);
         }
-        );
       });
-  
-/*  var retrieveImages = document.getElementById('imageRetrieve');
-  retrieveImages.onclick = function () {
-    var Retrieve = Parse.Object.extend("retrieve");
-    var query = new Parse.Query(register);
-    var self = this;
-    var name = this.$("#nameInput").val();
-    query.equalTo("name", name);
+
+    }
+
+    //Here we are calling the objectIds from parse, and setting them as the custom link for the a href tag
+    $("#list-forms").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr("href");
+      var query = new Parse.Query(Form);
+      query.equalTo("objectId", id);
+
+      query.include("user");
+
+      query.find({success: function (results) {
+        
+        var name = results[0].get("name");
+        var address = results[0].get("address");
+        var phone = results[0].get("phone");
+        var email = results[0].get("email");
+        var user = results[0].get("user");
+        var username = results[0].get("username");
+        var id = results[0].id;
+
+        //Here we are showing the image in our linked visible form
+        var src = "";
+        if (results[0].get("file")) {
+          src = results[0].get("file").url();
+        }
+
+        //Here we are telling the info which div to be shown in
+        $("#formLinkUser").html(user);
+        $("#formLinkName").html(name);
+        $("#formLinkAddress").html(address);
+        $("#formLinkPhone").html(phone);
+        $("#formLinkEmail").html(email);
+
+        //This assigns the objectId to the data-id in the html
+        $("#formLinkInfo").attr("data-id", id);
+        $("#formLinkImage").attr("src", src);
+        
+        //The formLinkInfo div is shown after clicking one of the links
+        div = document.getElementById("formLinkInfo");
+        div.style.display = "block";
+      },
+
+      error: function (error) {
+        console.log('Error with linking the ojectId, with ' + error.message);
+      }});
+    });
+
+    //This is where the info is being pulled & then shown in the html
+    console.log('Your info is being retrieved.');
+    var query = new Parse.Query(Form);
+
+    //query.include("user");
+
+    query.descending("createdAt");
+    query.limit(5);
     query.find({
       success: function (results) {
-        alert("Successfully retrieved " + results.length + ".");
-        imageURLs = [];
-        for (var i = 0; i < results.length; i++) {
-          var object = results[i];
-          var imageFile = object.get('image');
-          var imageURL = imageFile.url();
-          $('photoViewTest')[0].src = imageURL;
-        },
-        
-        error: function (error) {
-          alert("Error: " + error.code + " " + error.message);
+        console.log('Your info was retrieved successfully!');
+
+        var output = "";
+
+        for (var i in results) {
+          var name = results[i].get("name");
+          var address = results[i].get("address");
+          var phone = results[i].get("phone");
+          var email = results[i].get("email");
+          //var user = results[i].get("user");
+          //var username = user.get("username");
+
+          var id = results[i].id;
+
+          var img = "";
+          if (results[i].get("file")) {
+            var file = results[i].get("file");
+            var url = file.url();
+            console.log('url: ' + url);
+            img = "<img src'" +url+ "'>";
+          }
+          else {
+            console.log('There was no image selected.');
+          }
+
+          output += "<li>";
+          //output += "<h4>" +username+ "</h4>";
+          output += img;
+          output += "<h2><a href='" +id+ "'>" +name+ "</a></h2>";
+          output += "</li>";
         }
+
+        $("#list-forms").html(output);
+        
       },
+
+      error: function (error) {
+        console.log('There was a problem with retrieving your form. Error: ' + error.message);
+      }
     });
-  }, */
-  
+
+    //Clear the input fields
+    nameInput.value = "";
+    addressInput.value = "";
+    phoneInput.value = "";
+    emailInput.value = "";
+    imageInput.value = "";
+    
+    //The infoView div is shown after clicking submit
+    div = document.getElementById("infoView");
+    div.style.display = "block";
+
+  };
+
   //-----------
   //Collections
   //-----------
-  
+
   var FormList = Parse.Collection.extend({
     model: Form
   });
-  
-  //-----
-  //Views
-  //-----
-  
-  //Forms
-  var FormView = Parse.View.extend({
-    tagName: "li",
-    template: _.template($('#formViewTemplate').html()),
-    
-    initialize: function (options){
-      if (!(options && options.model))
-      throw new Error("model is not specified");
-      
-      this.model.on("change", this.render, this);
-    },
-    
-    render: function () {
-      //this sets the li's id attribute to the model's id
-      this.$el.attr("id", this.model.id);
-      $(this.el).html(this.template({
-        "photo_url" : this.model.photoUrl(),
-        "display_name" : this.model.displayName()
-      }));
-      
-      return this;
-    }
-  });
-  
-  //Recently Added Forms
-  var RecentFormsView = Parse.View.extend({
-    //Cache the template function for a single item
-    formsTemplate: _.template($('#recentFormTemplate').html()),
-    
-    initialize: function () {
-      var self = this;
-      _.bindAll(this, 'addOne', 'addAll', 'render');
-      
-      this.$el.html(this.formsTemplate);
-      
-      //Create our collection of forms
-      this.forms = new FormList();
-      
-      //Only show photos uploaded by me
-      var userQuery = new Parse.Query(Parse.User);
-      userQuery.containedIn("objectId", ["fTy19F7TMi", "RdjzMgKaQV", "zRwFjLhqlV", "5cD60MuVEE", "ZNcwbQwEhG", "42akJqaASJ", "lEPQ0nsHIO", "FXNvDAk5JL"]);
-      this.forms.query = new Parse.Query(Form);
-      this.forms.query.include("user");
-      this.forms.query.matchesQuery("user", userQuery);
-      
-      this.forms.query.limit(5);
-      this.forms.query.descending("createdAt");
-      
-      this.forms.bind('add', this.addOne);
-      this.forms.bind('reset', this.addAll);
-      this.forms.bind('all', this.render);
-      
-      //Fetch all the items for this user
-      this.forms.fetch();
-    },
-    
-    render: function () {
-      this.$("#form-list").fadeIn();
-      this.delegateEvents();
-      return this;
-    },
-    
-    addOne: function (form) {
-      var view = new FormView({model: form});
-      this.$("#form-list").append(view.render().el);
-    },
-    
-    //Add all items in the collection at once
-    addAll: function (collection, filter) {
-      this.forms.each(this.addOne);
-    }
-  });
-  
+
   //-----
   //Users
   //-----
-  
+
   var currentUser = Parse.User.current();
-    if (currentUser) {
-      Parse.User.logOut();
-    }
-  
-  //-------
-  //Routers
-  //-------
-  
+  if (currentUser) {
+    Parse.User.logOut();
+  }
+
   //Start Backbone History
   Backbone.history.start();
 });
